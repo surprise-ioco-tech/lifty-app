c6d07a729ca251cfc9e63248c24bbacf2e804690
ntsakosurprise <mashelesepru@gmail.com>
2023-06-01 16:54:42 +0200

    chore: properly configures the app

--- navigation/appnavigation.js
+++ navigation/appnavigation.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import LoginScreen from '../screens/login/login';
-import HomeScreen from '../screens/home/home';
-import {navigationRef} from '../../RootNavigation';
+import LoginScreen from "../screens/login/login";
+import HomeScreen from "../screens/home/home";
+import { navigationRef } from "../RootNavigation";
 
 const Stack = createNativeStackNavigator();
 
