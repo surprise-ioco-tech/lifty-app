d0dc13891bd53e02d9c2763fbed2a15f1a9b7c87
ntsakosurprise <mashelesepru@gmail.com>
2023-06-29 05:17:51 +0200

    chore: updates project with system files

--- android/app/src/main/java/com/surprisentsako/liftyapp/MainActivity.java
+++ android/app/src/main/java/com/surprisentsako/liftyapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.surprisentsako.liftyapp;
 import android.os.Build;
 import android.os.Bundle;
 
-import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
